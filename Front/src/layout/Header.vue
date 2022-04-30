71a3c0d7edc05b59906eabdb2f351d503143772e
Vincent Boni <vincent.boni@icloud.com>
2022-04-30 18:13:02 +0200

    IconButton qui fonctionne :)

--- Front/src/layout/Header.vue
+++ Front/src/layout/Header.vue
@@ -1,11 +1,28 @@
 <template>
     <div class="p-header">
-        Header
+        <IconButton
+            icon="fa-bars"
+            color="white"
+        />
+        <IconButton
+            icon="fa-home"
+            color="white"
+        />
+        <div class="p-header-title">
+            PandaGeek
+        </div>
     </div>
 </template>
 
 <script>
+    import IconButton from '../ui/IconButton.vue'
 
+    export default {
+        name: 'PlaygroungPage',
+        components: {
+            IconButton
+        }
+    }
 </script>
 
 <style scoped>
@@ -13,5 +30,12 @@
         padding: 10px;
         background-color: rebeccapurple;
         color: white;
+        display: flex;
+        align-items: center;
+    }
+    .p-header-title {
+        color: white;
+        font-weight: bold;
+        padding: 0px 10px;
     }
 </style>
--- Front/src/ui/IconButton.vue
+++ Front/src/ui/IconButton.vue
@@ -1,20 +1,56 @@
 <template>
     <div class="p-icon-button">
-        <i class="fas fa-bars"></i>
+        <i :class="[faClass, icon]"></i>
     </div>    
 </template>
 
 <script>
     export default {
+        props: [
+            'icon',
+            'color'
+        ],
         name: 'IconButton',
+        data() {
+            return {
+                faClass: 'fas',
+            }
+        },
+        computed: {
+            cssProps() {
+                return {
+                    'icon-color': (this.props.color) ? this.props.color : 'black',
+                }
+            },
+        }
     }
 </script>
 
 <style scoped>
-h1 {
+.p-icon-button {
+    padding: 10px;
+    border-radius: 100%;
+    display: inline-block;
+    width: 42px;
+    height: 42px;
+    box-sizing: border-box;
+    display: flex;
+    align-items: center;
+    transition: all 450ms cubic-bezier(0.23, 1, 0.32, 1) 0ms;
+    cursor: pointer;
+}
+.p-icon-button i {
+    color: var(icon-color);
+    width: 100%;
     text-align: center;
 }
-i {
-    color: black;
+.p-icon-button:hover {
+    background-color: rgba(210, 210, 210, 0.31);
+}
+.p-icon-button:active  {
+    transform: scale(0.95);
+}
+h1 {
+    text-align: center;
 }
 </style>
